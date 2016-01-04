8128979fe143fc296f55cf69d574ac3769394856
alex <alex@alex-Latitude-E5440.(none)>
2016-01-04 17:33:00 +0800

    android building wireless_tools_29

---
+++ wireless_tools_29/build.sh
@@ -0,0 +1,4 @@
+export PATH=/home/alex/work/source/a64/lichee/out/sun50iw1p1/android/common/buildroot/external-toolchain/bin:$PATH
+./configure --host=aarch64-linux-gnu --prefix=/home/alex/work/source/a64/android/external/wpa_supplicant_8/iperf-2.0.5/bin
+#source ./configure --host=arm-none-linux-gnueabi
+#source ./configure --host=aarch64-linux-android
